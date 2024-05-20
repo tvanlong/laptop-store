ab51b02aaf371047701bc496a57f438af3bd7452
tvanlong <longvan1173@gmail.com>
2024-05-20 11:40:24 +0700

    feat: Improve navigation in SubHeader component

--- src/components/SubHeader/SubHeader.jsx
+++ src/components/SubHeader/SubHeader.jsx
@@ -1,14 +1,20 @@
+import { Link } from 'react-router-dom'
+
 function SubHeader() {
   return (
     <header className='h-[88px]'>
       <div className='bg-[#242525] h-[88px] fixed top-0 left-0 right-0 z-20'>
         <div className='h-full max-w-[95%] m-auto flex items-center justify-between gap-8'>
-          <div className='font-semibold text-white cursor-pointer hover:underline'>Trở về</div>
-          <img
-            className='cursor-pointer'
-            src='https://laptopkhanhtran.vn/pic/banner/logo_6368_638173418442942155.png'
-            alt=''
-          />
+          <Link to='/' className='font-semibold text-white cursor-pointer hover:underline'>
+            Trở về
+          </Link>
+          <Link to='/'>
+            <img
+              className='cursor-pointer'
+              src='https://laptopkhanhtran.vn/pic/banner/logo_6368_638173418442942155.png'
+              alt='logo'
+            />
+          </Link>
         </div>
       </div>
     </header>
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -119,10 +119,27 @@ function Login({ setProgress }) {
             </svg>
           </button>
           <button
-            className='bg-[#ec2127] w-full p-4 rounded-md hover:opacity-80 mt-8'
+            className='bg-[#ec2127] flex items-center justify-center w-full p-4 rounded-md hover:opacity-80 mt-8'
             onClick={() => navigate(path.register)}
           >
             <span className='text-white font-medium text-xl uppercase'>Đăng ký</span>
+            <svg
+              className='w-6 h-6 text-white ml-3'
+              aria-hidden='true'
+              xmlns='http://www.w3.org/2000/svg'
+              width='24'
+              height='24'
+              fill='none'
+              viewBox='0 0 24 24'
+            >
+              <path
+                stroke='currentColor'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d='M12 7.757v8.486M7.757 12h8.486M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z'
+              />
+            </svg>
           </button>
         </form>
         <div className='relative py-6 flex justify-center'>

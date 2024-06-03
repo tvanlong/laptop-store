70177419b19429e32cf0f6aec0e031fb2281c2a3
tvanlong <longvan1173@gmail.com>
2024-06-03 16:45:28 +0700

    refactor: Update user.api.js to use new endpoint for updating profile and changing password

--- src/apis/user.api.js
+++ src/apis/user.api.js
@@ -2,6 +2,6 @@ import http from '~/utils/http'
 
 export const getProfile = (id) => http.get(`/api/users/customers/${id}`)
 
-export const updateProfile = (id, data) => http.patch(`/api/users/customers/${id}`, data)
+export const updateProfile = (id, data) => http.patch(`/api/users/update-profile/${id}`, data)
 
-export const changePassword = (id, data) => http.patch(`/api/users/customers/${id}/change-password`, data)
+export const changePassword = (id, data) => http.patch(`/api/users/change-password/${id}`, data)

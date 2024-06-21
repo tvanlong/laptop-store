213b92191be32c5dc8f73c020063f14d555bedf9
tvanlong <longvan1173@gmail.com>
2024-06-21 16:03:03 +0700

    refactor: Update API root URL for production build

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,6 +1,6 @@
 let apiRoot = ''
 if (process.env.BUILD_MODE === 'prod') {
-  apiRoot = 'https://laptop-kt-store.up.railway.app'
+  apiRoot = 'https://laptop-store-api.onrender.com'
 }
 if (process.env.BUILD_MODE === 'dev') {
   apiRoot = 'http://localhost:3000'

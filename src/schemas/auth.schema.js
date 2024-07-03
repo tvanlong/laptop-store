8dcf53cf80c147ba2bb8b5f71f53cd31f0edb46d
tvanlong <longvan1173@gmail.com>
2024-07-03 21:17:30 +0700

    refactor: Update validation rules for name and phone in signUpSchema

--- src/schemas/auth.schema.js
+++ src/schemas/auth.schema.js
@@ -6,8 +6,11 @@ export const signInSchema = yup.object({
 })
 
 export const signUpSchema = yup.object({
-  name: yup.string().required('Họ tên không được để trống'),
-  phone: yup.string().required('Số điện thoại không được để trống'),
+  name: yup.string().min(6, 'Tên phải có ít nhất 6 ký tự').required('Tên không được để trống'),
+  phone: yup
+    .string()
+    .matches(/(84|0[3|5|7|8|9])+([0-9]{8})\b/, 'Số điện thoại không hợp lệ')
+    .required('Số điện thoại không được để trống'),
   email: yup.string().email('Email không hợp lệ').required('Email không được để trống'),
   password: yup.string().min(6, 'Mật khẩu phải có ít nhất 6 ký tự').required('Mật khẩu không được để trống'),
   confirm_password: yup.string().oneOf([yup.ref('password'), null], 'Mật khẩu không khớp')

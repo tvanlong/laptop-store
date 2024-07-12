6d22cdf549674a598728ecf934ba3730ba147ae0
tvanlong <longvan1173@gmail.com>
2024-07-12 21:35:12 +0700

    refactor: Update component to handle opening and closing of the sidebar menu

--- src/components/SidebarMenu/SidebarMenu.jsx
+++ src/components/SidebarMenu/SidebarMenu.jsx
@@ -35,68 +35,76 @@ function SidebarMenu() {
   }
 
   return (
-    <div
-      className={
-        isOpenSidebarMenu
-          ? 'fixed top-[144px] left-0 w-64 text-white h-full bg-[#242525] z-50 shadow-lg transition-transform transform duration-300 ease-in-out overflow-y-auto no-scrollbar'
-          : 'fixed top-[144px] left-0 w-64 text-white h-full bg-[#242525] z-50 shadow-lg transition-transform transform duration-300 ease-in-out -translate-x-full overflow-y-auto no-scrollbar'
-      }
-    >
-      <div className='flex items-center justify-between h-16 px-4 bg-gray-700 border-b border-gray-600'>
-        <span className='text-white font-bold text-lg'>Danh mục sản phẩm</span>
-        <button
-          className='text-white border border-white p-1 rounded-full hover:text-gray-300 focus:outline-none'
+    <div className='relative'>
+      {isOpenSidebarMenu && (
+        <div
+          className='fixed inset-0 bg-black opacity-50 z-40 pointer-events-auto'
           onClick={() => setIsOpenSidebarMenu(false)}
-        >
-          <svg
-            className='w-4 h-4'
-            fill='none'
-            stroke='currentColor'
-            viewBox='0 0 24 24'
-            xmlns='http://www.w3.org/2000/svg'
+        ></div>
+      )}
+      <div
+        className={
+          isOpenSidebarMenu
+            ? 'fixed top-[144px] left-0 w-64 text-white h-[calc(100vh-144px)] bg-[#242525] z-50 shadow-lg transition-transform transform duration-300 ease-in-out overflow-y-auto no-scrollbar'
+            : 'fixed top-[144px] left-0 w-64 text-white h-[calc(100vh-144px)] bg-[#242525] z-50 shadow-lg transition-transform transform duration-300 ease-in-out -translate-x-full overflow-y-auto no-scrollbar'
+        }
+      >
+        <div className='flex items-center justify-between h-16 px-4 bg-gray-700 border-b border-gray-600'>
+          <span className='text-white font-bold text-lg'>Danh mục sản phẩm</span>
+          <button
+            className='text-white border border-white p-1 rounded-full hover:text-gray-300 focus:outline-none'
+            onClick={() => setIsOpenSidebarMenu(false)}
           >
-            <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M15 19l-7-7 7-7' />
-          </svg>
-        </button>
-      </div>
-      <ul className='mt-6'>
-        {categories.map((category) => (
-          <li key={category._id}>
-            <div
-              className={
-                pathname.includes(category._id)
-                  ? 'px-4 py-2 flex items-center justify-between text-gray-200 border-l-4 border-transparent bg-gray-700 border-gray-600 cursor-pointer'
-                  : 'px-4 py-2 flex items-center justify-between text-gray-200 border-l-4 border-transparent hover:bg-gray-700 hover:border-gray-600 cursor-pointer'
-              }
+            <svg
+              className='w-4 h-4'
+              fill='none'
+              stroke='currentColor'
+              viewBox='0 0 24 24'
+              xmlns='http://www.w3.org/2000/svg'
             >
-              <span className='block' onClick={() => handleNavigateCategory(category._id)}>
-                {category.name}
-              </span>
-              {category.subcategories.length > 0 && (
-                <button
-                  className='text-gray-400 rounded-full border border-gray-400 h-6 w-6 flex items-center justify-center focus:outline-none'
-                  onClick={() => handleCategoryClick(category._id)}
-                >
-                  <span className=''>{expandedCategories[category._id] ? '-' : '+'}</span>
-                </button>
-              )}
-            </div>
-            {expandedCategories[category._id] && (
-              <ul className='pl-8 mt-2'>
-                {category.subcategories.map((subcategory) => (
-                  <li
-                    key={subcategory._id}
-                    className={`pl-4 py-2 text-gray-200 border-l-4 border-transparent cursor-pointer ${pathname.includes(subcategory._id) ? 'bg-gray-700 border-gray-600' : 'hover:bg-gray-700 hover:border-gray-600'}`}
-                    onClick={() => handleSubcategoryClick(category._id, subcategory._id)}
+              <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M15 19l-7-7 7-7' />
+            </svg>
+          </button>
+        </div>
+        <ul className='mt-6'>
+          {categories.map((category) => (
+            <li key={category._id}>
+              <div
+                className={
+                  pathname.includes(category._id)
+                    ? 'px-4 py-2 flex items-center justify-between text-gray-200 border-l-4 border-transparent bg-gray-700 border-gray-600 cursor-pointer'
+                    : 'px-4 py-2 flex items-center justify-between text-gray-200 border-l-4 border-transparent hover:bg-gray-700 hover:border-gray-600 cursor-pointer'
+                }
+              >
+                <span className='block' onClick={() => handleNavigateCategory(category._id)}>
+                  {category.name}
+                </span>
+                {category.subcategories.length > 0 && (
+                  <button
+                    className='text-gray-400 rounded-full border border-gray-400 h-6 w-6 flex items-center justify-center focus:outline-none'
+                    onClick={() => handleCategoryClick(category._id)}
                   >
-                    <span className='block'>{subcategory.name}</span>
-                  </li>
-                ))}
-              </ul>
-            )}
-          </li>
-        ))}
-      </ul>
+                    <span className=''>{expandedCategories[category._id] ? '-' : '+'}</span>
+                  </button>
+                )}
+              </div>
+              {expandedCategories[category._id] && (
+                <ul className='pl-8 mt-2'>
+                  {category.subcategories.map((subcategory) => (
+                    <li
+                      key={subcategory._id}
+                      className={`pl-4 py-2 text-gray-200 border-l-4 border-transparent cursor-pointer ${pathname.includes(subcategory._id) ? 'bg-gray-700 border-gray-600' : 'hover:bg-gray-700 hover:border-gray-600'}`}
+                      onClick={() => handleSubcategoryClick(category._id, subcategory._id)}
+                    >
+                      <span className='block'>{subcategory.name}</span>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
